699ee78ba52dc13719c1d329354935fc9b52df54
matheus-reyes <matheuscreyes@gmail.com>
2021-10-11 18:54:47 -0300

    feat: adding services page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/inicioCliente', function(req, res, next) {
+  res.render('inicioCliente', { title: 'Express' });
+});
+
 module.exports = router;
