7799cb8c6d498b3202eb0237387da5d0404fd503
Nanda Cavalcante <fecavalcante2010@gmail.com>
2021-10-15 18:02:52 -0300

    telas de servicos

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/inicioCliente', function(req, res, next) {
   res.render('inicioCliente', { title: 'Express' });
 });
 
+router.get('/servicos', function(req, res, next) {
+  res.render('servicos', { title: 'Express' });
+});
+
 module.exports = router;
